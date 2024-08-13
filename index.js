d2c5cc790709a660bb7d632d512e25df11b63456
Lucas Miranda <mirandalucasemanuel@gmail.com>
2024-08-13 20:29:51 -0300

    Projeto realizado em HTML, CSS e JS, para uma amiga divulgar seus trabalhos como personal trainner!

---
+++ index.js
@@ -0,0 +1,8 @@
+function openWhatsapp () {
+    window.open("https://wa.me/5531997184660", "_blank")
+}
+
+
+function saibaMais() {
+    window.open("https://www.instagram.com/circuito_academia_/")
+}
